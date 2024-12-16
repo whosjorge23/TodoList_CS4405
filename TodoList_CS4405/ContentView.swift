acc9ad66dd054c19f086a94cbe7898d7a77fa49e
Giorgio Giannotta <jorge_upa@msn.com>
2024-12-16 07:56:16 +0700

    Initial Commit

---
+++ TodoList_CS4405/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  TodoList_CS4405
+//
+//  Created by Giorgio Giannotta on 16/12/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ TodoList_CS4405/TodoList_CS4405App.swift
@@ -0,0 +1,17 @@
+//
+//  TodoList_CS4405App.swift
+//  TodoList_CS4405
+//
+//  Created by Giorgio Giannotta on 16/12/24.
+//
+
+import SwiftUI
+
+@main
+struct TodoList_CS4405App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
